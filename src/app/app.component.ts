dfbede7fa7c4607769c5554a749bf5b148075212
Javier Lete <jglete@gmail.com>
2019-04-02 20:54:58 +0200

    Ejemplos de ngFor e ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   color = 'blue';
 
+  heroes = [ { name: 'uno' }, { name: 'dos' } ];
+
   setColor(color: string) {
     this.color = color;
   }
