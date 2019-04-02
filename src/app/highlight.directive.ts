e61eb92eb2a21a93f1caa9ac3d88b7b8b34786b9
Javier Lete <jglete@gmail.com>
2019-04-02 18:54:27 +0200

    Directiva con modificación de estilo y contenido

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnInit {
+  ngOnInit(): void {
+    this.el.nativeElement.style.backgroundColor = "yellow";
+    console.log(this.el.nativeElement.innerHTML);
+    this.el.nativeElement.innerHTML = '||' + this.el.nativeElement.innerHTML + '||';
+    console.log(this.el.nativeElement.innerHTML);
+  }
+
+  constructor(private el: ElementRef) {  }
+
+}
